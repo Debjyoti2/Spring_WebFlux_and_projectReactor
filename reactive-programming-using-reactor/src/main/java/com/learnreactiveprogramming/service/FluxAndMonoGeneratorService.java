4fdcc52047fb7c0d1a292177a2af82d4d7093323
Debjyoti Roychoudhury <debjyoti.10728458@ltimindtree.com>
2023-11-08 09:35:50 +0530

    adding first flux

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,17 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> empNames(){
+        return Flux.fromIterable(List.of("Joydeep","Soutan","Debjyoti","Loknath"));
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.empNames().subscribe(name-> System.out.println("Name is : " + name));
+    }
 }
