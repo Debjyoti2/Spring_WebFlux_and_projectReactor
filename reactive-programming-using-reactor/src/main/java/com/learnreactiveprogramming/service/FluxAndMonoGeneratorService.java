c1e4647d188cc311448de7d0361a48a77a8e8bd5
debjyoti2 <debjyotiroychoudhury2012@gmail.com>
2023-11-09 08:31:04 +0530

    modified first flux

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -7,11 +7,13 @@ import java.util.List;
 public class FluxAndMonoGeneratorService {
 
     public Flux<String> empNames(){
+
         return Flux.fromIterable(List.of("Joydeep","Soutan","Debjyoti","Loknath"));
     }
 
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
-        fluxAndMonoGeneratorService.empNames().subscribe(name-> System.out.println("Name is : " + name));
+        fluxAndMonoGeneratorService.empNames()
+                .subscribe(name-> System.out.println("Name is : " + name));
     }
 }
